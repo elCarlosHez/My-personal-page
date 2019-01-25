1425bcb2c2318b6b2fcae565dff46b411a6b2fef
Carlos Arellano <luis.harellano@alumnos.udg.mx>
2019-01-25 17:43:06 -0600

    Smooth scroll for all browsers

--- js/main.js
+++ js/main.js
@@ -1,31 +1,11 @@
-AOS.init({
-    disable: 'mobile',
-});
 
-function animate(elem, style, unit, from, to, time, prop) {
-    if (!elem) {
-        return;
-    }
-    var start = new Date().getTime(),
-        timer = setInterval(function () {
-            var step = Math.min(1, (new Date().getTime() - start) / time);
-            if (prop) {
-                elem[style] = (from + step * (to - from))+unit;
-            } else {
-                elem.style[style] = (from + step * (to - from))+unit;
-            }
-            if (step === 1) {
-                clearInterval(timer);
-            }
-        }, 25);
-    if (prop) {
-    	  elem[style] = from+unit;
-    } else {
-    	  elem.style[style] = from+unit;
-    }
-}
+window.addEventListener('load',function(e){
+    AOS.init({
+        disable: 'mobile',
+    });
+    
+    document.getElementById('scroll-down').addEventListener('click',e => {
+        window.scroll({ top: document.getElementById('abaout').offsetTop + 50, left: 0, behavior: 'smooth' });
+    });
+})
 
-document.getElementById('scroll-down').addEventListener('click',e => {
-    let target = document.getElementById("abaout");
-    animate(document.scrollingElement || document.documentElement, "scrollTop", "", 0, target.offsetTop, 1200, true);
-});
